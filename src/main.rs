f8573db00ff7da6d5a54951892b7ccca2ed8b12b
Ryan1729 <Ryan1729@gmail.com>
2023-12-30 00:06:00 -0700

    extract file entries, but find they don't extend to the next DSARCIDX

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ use std::io::{
     Write,
 };
 
+use core::mem::size_of;
+
+macro_rules! compile_time_assert {
+    ($assertion: expr) => {
+        #[allow(unknown_lints, clippy::eq_op)]
+        // Based on the const_assert macro from static_assertions.
+        const _: [(); 0 - !{$assertion} as usize] = [];
+    }
+}
+
 type Res<A> = Result<A, Box<dyn std::error::Error>>;
 
 struct HexSlice<'slice>(&'slice [u8]);
@@ -35,42 +45,135 @@ fn main() -> Res<()> {
     );
 
     type Addr = u32;
+    type AddrOffset = u32;
 
-    const DSARC_FL_START: Addr = 0x000C_829C;
-    const DSARC_FL_MAGIC: &[u8] = b"DSARC FL\0";
-    const DSARCIDX_MAGIC: &[u8] = b"DSARCIDX\0";
+    const FIRST_DSARCIDX_START: Addr = 0x000F_C600;
 
-    rom.seek(SeekFrom::Start(u64::from(DSARC_FL_START)))?;
+    mod dsarcidx {
+        pub const MAGIC: &[u8] = b"DSARCIDX";
+    }
+
+    rom.seek(SeekFrom::Start(u64::from(FIRST_DSARCIDX_START)))?;
 
     {
-        let mut buffer = [0; DSARC_FL_MAGIC.len()];
+        let mut buffer = [0; dsarcidx::MAGIC.len()];
 
         rom.read_exact(&mut buffer)?;
 
-        if buffer != DSARC_FL_MAGIC {
+        if buffer != dsarcidx::MAGIC {
             Err(format!(
                 "Expected {} at {:#010X}, got {}",
-                HexSlice(DSARC_FL_MAGIC),
+                HexSlice(dsarcidx::MAGIC),
                 Addr::try_from(rom.position())?,
                 HexSlice(&buffer),
             ))?;
         }
     }
 
+    // Advance to the end of the header 
+    let entry_count;
     {
-        let mut buffer = [0; DSARCIDX_MAGIC.len()];
+        macro_rules! read_u32 {
+            () => ({
+                let mut buffer = [0; 4];
+        
+                rom.read_exact(&mut buffer)?;
+        
+                u32::from_le_bytes(buffer)
+            })
+        }
+
+        entry_count = read_u32!();
+dbg!(entry_count);
+        assert!(entry_count > 0);
+
+        // A field of unknown purpose which is always 0.
+        // (Maybe reserved for a file type version number?)
+        let _version = read_u32!();
+
+        let ids_length: AddrOffset = 
+            // 2 bytes for each ID
+            2 * entry_count;
+
+        rom.seek(SeekFrom::Current(
+            i64::from(ids_length)
+        ))?;
+
+        rom.set_position(
+            (
+                rom.position() 
+                // Add and AND to align to 4 byte boundary
+                + (4 - 1)
+            ) & !(4 - 1)
+        );
+        
+    }
+
+    type FileName = [u8; 40];
+
+    #[repr(C)]
+    #[derive(Debug)]
+    struct FileEntry<const BASE: Addr> {
+        file_name: FileName,
+        size: AddrOffset,
+        offset: AddrOffset,
+    }
+
+    impl <const BASE: Addr> FileEntry<BASE> {
+        fn addr(&self) -> Addr {
+            BASE + self.offset
+        }
+
+        fn after(&self) -> Addr {
+            self.addr() + self.size
+        }
+    }
+
+    // Assert that there is no struct padding
+    compile_time_assert!{
+        size_of::<FileEntry<0>>()
+        == (
+            size_of::<FileName>()
+            + size_of::<u32>()
+            + size_of::<u32>()
+        )
+    }
+
+    let mut entries: Vec<FileEntry<FIRST_DSARCIDX_START>>
+        // + 1 in case we need to add an entry for
+        = Vec::with_capacity((entry_count + 1) as _);
+
+    for _ in 0..entry_count {
+        let mut buffer = [0u8; size_of::<FileEntry<0>>()];
 
         rom.read_exact(&mut buffer)?;
 
-        if buffer != DSARCIDX_MAGIC {
-            Err(format!(
-                "Expected {} at {:#010X}, got {}",
-                HexSlice(DSARCIDX_MAGIC),
-                Addr::try_from(rom.position())?,
-                HexSlice(&buffer),
-            ))?;
+        // SAFETY: FileEntry has no padding, and all bit values are valid.
+        let entry = unsafe {
+            core::mem::transmute::<
+                [u8; size_of::<FileEntry<FIRST_DSARCIDX_START>>()],
+                FileEntry<FIRST_DSARCIDX_START>,
+            >(buffer)
+        };
+
+        entries.push(entry);
+    }
+
+    assert_eq!(entries.len(), entry_count as _);
+
+    let mut max_after: Addr = 0;
+
+    for entry in entries {
+        let after = entry.after();
+dbg!(std::str::from_utf8(&entry.file_name), after);
+        if after > max_after {
+            max_after = after;
         }
     }
+
+    println!(
+        "{max_after:#010X}"
+    );
 
     std::fs::write(output_path, rom.get_ref())
         .map_err(From::from)
